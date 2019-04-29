09541fa38125032cd49f0c186e0d4ba368e89364
Alexia Johnson <alexiajohnson4019@gmail.com>
2019-04-28 19:18:03 -0700

    make production build available for github pages

---
+++ build/precache-manifest.05a0aaeea1f162699492028471f161e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11852a752f31d36d2d1f783085022459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c2a139d53add51596f6",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9c2a139d53add51596f6",
+    "url": "/static/js/main.35a02bc2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4aac6085e1e2f3d55d12f9ad02e49f5b",
+    "url": "/static/media/RainbowonWhiteLG.4aac6085.jpg"
+  }
+]);
